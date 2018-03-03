51f1836ba472b531c5f972061fe7fb4e0343df3e
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-03 10:06:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,17 @@
 listPool = [
+  ['Stellite', 'http://npool.pw/stellite', 'http://stl.npool.pw:8113'],
+  ['Pinkstarcoin', 'http://npool.pw/pnks', 'http://pnks.npool.pw:8114'],
+  ['BBScoin', 'http://npool.pw/bbscoin', 'http://bbs.npool.pw:8115'],
+  ['Qwertycoin', 'http://npool.pw/qwc', 'http://qwc.npool.pw:8116'],
   ['IPBC', 'http://npool.pw/ipbc', 'http://ipbc.pool.ahka.net:8117'],
-  ['Triton', 'http://npool.pw/triton', 'http://trtn.npool.pw:10010']
+  ['Crepcoin', 'http://npool.pw/crepcoin', 'http://crep.npool.pw:8118'],
+  ['Intensecoin', 'http://npool.pw/itns', 'http://itns.npool.pw:8119'],
+  ['Arto', 'http://npool.pw/arto', 'http://rto.npool.pw:10000'],
+  ['KreditX', 'http://npool.pw/kreditx', 'http://krx.npool.pw:10030'],
+  ['Tychecash', 'http://npool.pw/tychecash', 'http://tyc.npool.pw:10020'],
+  ['Balkancoin', 'http://npool.pw/bkc', 'http://bkc.npool.pw:10040'],
+  ['Loncoin', 'http://npool.pw/loncoin', 'http://lcn.npool.pw:10050'],
+  ['Jimcoin', 'http://npool.pw/jimcoin', 'http://jim.npool.pw:10060'],
+  ['Triton', 'http://npool.pw/triton', 'http://trtn.npool.pw:10010'],
+  ['Bitsum', 'http://npool.pw/bitsum', 'http://sum.npool.pw:10070']
 ];
