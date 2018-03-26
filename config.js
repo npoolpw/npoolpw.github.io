30ebafb32250f95e504334ebe3536fbe43101393
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-26 23:10:25 +0800

    Update config.js

--- config.js
+++ config.js
@@ -20,5 +20,6 @@ listPool = [
   ['Marketcash', 'http://npool.pw/marketcash', 'http://ca.npool.pw:2230'],
   ['Linescoin', 'http://npool.pw/linescoin', 'http://ca.npool.pw:2240'],
   ['Halykcoin', 'http://npool.pw/halykcoin', 'http://ca.npool.pw:2250'],
-  ['Spescoin', 'http://npool.pw/spescoin', 'http://ca.npool.pw:2260']
+  ['Spescoin', 'http://npool.pw/spescoin', 'http://ca.npool.pw:2260'],
+  ['Solcash', 'http://npool.pw/solcash', 'http://ca.npool.pw:2280']
 ];
