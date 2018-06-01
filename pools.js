e4a49c45d58931d4ff1f9285b99c13d86e7409b0
leisegang <leisegang@gmail.com>
2018-06-01 23:43:05 +0200

    updated url to official pool

--- pools.js
+++ pools.js
@@ -1,5 +1,5 @@
 listPool = [
-  ['Poolmining', 'http://poolmining.us/', 'http://poolmining.us:7111'],
+  ['pinkstarcoin.com', 'http://pool.pinkstarcoin.com/', 'http://pool.pinkstarcoin.com:7111'],
   ['nPool', 'http://npool.pw/pstar', 'http://pnks.npool.pw:8114'],
   ['5wa', 'http://pstar.5wa.io/en', 'http://pstar.5wa.io:8117']
 ];
