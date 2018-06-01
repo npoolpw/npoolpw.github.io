3dc93c9046cc1962e5444ec822c2380830078b25
leisegang <leisegang@gmail.com>
2018-06-01 23:23:19 +0200

    added pool

--- pools.js
+++ pools.js
@@ -1,4 +1,5 @@
 listPool = [
   ['Poolmining', 'http://poolmining.us/', 'http://poolmining.us:7111'],
-  ['nPool', 'http://npool.pw/pstar', 'http://pnks.npool.pw:8114']
+  ['nPool', 'http://npool.pw/pstar', 'http://pnks.npool.pw:8114'],
+  ['5wa', 'http://pstar.5wa.io/en', 'http://pstar.5wa.io:8117']
 ];
